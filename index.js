9569c07e76b5956d5360eda9a37148180f57049a
Divyesh2Suhagiya <147687876+Divyesh2Suhagiya@users.noreply.github.com>
2023-12-11 16:53:01 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
-const express = require('express')
+const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
 
+mongoose.connect('mongodb+srv://suhagiyadivyesh:DIvyesh%40Mongodb@cluster0.w6fsp.mongodb.net/employee_System?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+}).then(() => {
+    console.log("Successfully connected to the database");
+}).catch((error) => {
+    console.log("Database connection failed. Exiting now...");
+    console.log(error);
+});
 
 app.get('/', (req,res)=>{
     return res.send('Welcome!')
